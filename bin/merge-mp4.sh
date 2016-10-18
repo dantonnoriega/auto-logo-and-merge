9f1320f274a8af0b076fbd49ccf108e38b07392e
ultinomics <ultinomics@github.com>
2016-10-18 03:20:57 -0700

    default to 1920x1080

--- bin/merge-mp4.sh
+++ bin/merge-mp4.sh
@@ -11,11 +11,11 @@ echo "AUDIO = $AUDIO"
 # generate static, 10 sec still
 # NOTE input are relative but OUTPUTS must have a './'
 ffmpeg -y -loop 1 -i img/logo_static.png -c:v libx264 \
-  -t 10 -pix_fmt yuv420p -vf scale=1280:720 mp4/02-logo-static.mp4;
+  -t 10 -pix_fmt yuv420p -vf scale=1920:1080 mp4/02-logo-static.mp4;
 
 # convert the movie clip
 ffmpeg -y -i $VIDEO -c:v libx264 \
-  -pix_fmt yuv420p -vf scale=1280:720 mp4/01-video-recode.mp4;
+  -pix_fmt yuv420p -vf scale=1920:1080 mp4/01-video-recode.mp4;
 
 # check for file list. remove if exists.
 if \[ -f files.txt \];
